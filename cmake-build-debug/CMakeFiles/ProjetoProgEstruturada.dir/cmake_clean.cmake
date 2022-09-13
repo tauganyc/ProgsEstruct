13f20287494778d873925fc9d226dc30da57e15f
Taugany Corrêa de Souza <tauganyc@hotmail.com>
2022-09-13 11:35:55 -0300

    att atta

---
+++ cmake-build-debug/CMakeFiles/ProjetoProgEstruturada.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetoProgEstruturada.dir/funcoes.c.o"
+  "CMakeFiles/ProjetoProgEstruturada.dir/funcoes.c.o.d"
+  "CMakeFiles/ProjetoProgEstruturada.dir/main.c.o"
+  "CMakeFiles/ProjetoProgEstruturada.dir/main.c.o.d"
+  "ProjetoProgEstruturada"
+  "ProjetoProgEstruturada.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetoProgEstruturada.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
